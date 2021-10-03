13a6c882e6f10cd3bfcb05207f13cd18b82091f7
Kingsley Christudoss <jaykings@gmail.com>
2021-10-03 07:13:22 +0000

    Implementation of the Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
+function compute() // Function to calculate interest earned over the specified period or raises an alert if principal is 0 or less
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal > 0) {
+        var rate = document.getElementById("rate").value;
+        var years= document.getElementById("years").value;
+        var interest = (principal * (rate / 100) * years).toFixed(2); // Round value to 2 decimal points for currency representation
+        // var interest = principal * (rate / 100) * years;
+	var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="\<strong\>If</strong> you deposit \<mark\>"+principal+"\</mark\>,\<br\>\<strong\>at\</strong\> an interest rate of \<mark\>"+rate+"%\</mark\>.\<br\>\<strong\>You\</strong\> will recieve an amount of \<mark\>"+interest+"\</mark\>,\<br\>\<strong\>in\</strong\> the year of \<mark\>"+year+"\</mark\>.";
+    }
+    else {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;    
+    }
+    return true
 }
-        
+function updateRate() // Function to clear previous calculation and display rate selected
+{
+    clearResult();
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval+"%";
+}
+function clearResult() // Function to clear previous calculation
+{
+    document.getElementById("result").innerHTML="";
+}
